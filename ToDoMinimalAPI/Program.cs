6400f04472eb4a24fddbe80b0394468512b24993
Sina <sinazare1381@gmail.com>
2023-10-17 19:39:35 +0330

    Added other Dependencies like swagger auth or HealthChecks to Minimal API in Extension methods

--- ToDoMinimalAPI/Program.cs
+++ ToDoMinimalAPI/Program.cs
@@ -1,37 +1,14 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using ToDoLibrary.DataAccess;
 using ToDoMinimalAPI.Endpoints;
+using ToDoMinimalAPI.StartupConfig;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
-builder.Services.AddSingleton<ITodoData, TodoData>();
 
-builder.Services.AddAuthorization(opts =>
-{
-    opts.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
-});
-
-builder.Services.AddAuthentication("Bearer").AddJwtBearer(opts =>
-{
-    opts.TokenValidationParameters = new()
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
-        ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.ASCII.GetBytes(
-                builder.Configuration.GetValue<string>("Authentication:SecretKey")))
-    };
-});
+builder.AddStandardServices();
+builder.AddDataAccessServices();
+builder.AddAuthServices();
+builder.AddHealthCheckServices();
 
 var app = builder.Build();
 
@@ -48,5 +25,7 @@ app.UseAuthorization();
 
 app.AddAuthenicationEndpoint();
 app.AddTodosEndpoints();
+
+app.MapHealthChecks("/health").AllowAnonymous();
 
 app.Run();
---
+++ ToDoMinimalAPI/StartupConfig/DependencyInjectionExtensions.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text;
+using ToDoLibrary.DataAccess;
+
+namespace ToDoMinimalAPI.StartupConfig;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddStandardServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.AddSwaggerServices();
+    }
+
+    private static void AddSwaggerServices(this WebApplicationBuilder builder)
+    {
+        var securityScheme = new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Description = "JWT Authorization header info using bearer tokens",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT"
+        };
+
+        var securityRequirement = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "bearerAuth"
+                        }
+                    },
+                    new string[] {}
+                }
+            };
+
+        builder.Services.AddSwaggerGen(opts =>
+        {
+            opts.AddSecurityDefinition("bearerAuth", securityScheme);
+            opts.AddSecurityRequirement(securityRequirement);
+        });
+    }
+
+    public static void AddAuthServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthorization(opts =>
+        {
+            opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+        });
+
+        builder.Services.AddAuthentication("Bearer").AddJwtBearer(opts =>
+        {
+            opts.TokenValidationParameters = new()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+                ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.ASCII.GetBytes(
+                        builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+            };
+        });
+    }
+
+    public static void AddDataAccessServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+        builder.Services.AddSingleton<ITodoData, TodoData>();
+    }
+
+    public static void AddHealthCheckServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddHealthChecks()
+            .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
+    }
+}
